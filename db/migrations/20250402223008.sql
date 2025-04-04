a74abd07aa79ee65ea38fac52973ae18d4419e58
Yusuke Saito <info@ysaito.dev>
2025-04-04 22:23:54 +0900

    amend

---
+++ db/migrations/20250402223008.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+  Persons (FirstName, LastName, Address, City, State, ZipCode, CreatedAt, UpdatedAt)
+VALUES
+	('John',    'Doe',     '123 Elm St',   'Springfield', 'IL', '62701', "2025-04-03T00:00:00Z", "2025-04-03T00:00:00Z"),
+	('Jane',    'Smith',   '456 Oak St',   'Springfield', 'IL', '62702', "2025-04-03T00:00:00Z", "2025-04-03T00:00:00Z"),
+	('Alice',   'Johnson', '789 Pine St',  'Springfield', 'IL', '62703', "2025-04-03T00:00:00Z", "2025-04-03T00:00:00Z"),
+	('Bob',     'Brown',   '101 Maple St', 'Springfield', 'IL', '62704', "2025-04-03T00:00:00Z", "2025-04-03T00:00:00Z"),
+	('Charlie', 'Davis',   '202 Birch St', 'Springfield', 'IL', '62705', "2025-04-03T00:00:00Z", "2025-04-03T00:00:00Z")
+;
