d88b11bd90645dd132152a75fab8263bbfe3a1c9
Yusuke Saito <info@ysaito.dev>
2025-04-17 14:45:22 +0900

    amend

--- handler/repository.rb
+++ handler/repository.rb
@@ -1,5 +1,5 @@
 require 'mysql2'
 
 client = Mysql2::Client.new(host: env['DB_HOST'])
-results = client.query("SELECT * FROM Persons WHERE FirstName = 'John'")
+results = client.query("SELECT * FROM People WHERE FirstName = 'John'")
 puts results.to_a
